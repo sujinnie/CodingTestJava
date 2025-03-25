31f98755e589cdd2c9a069231c7a9b00f66ffa3a
Sujin Jeong <sujin5262@naver.com>
2025-03-25 19:17:59 +0900

    [level 1] Title: K번째수, Time: 0.47 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,12 +4,14 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         for(int i=0; i<commands.length; i++) {
-            int k=0;
-            int[] slicedArr = new int[commands[i][1] - commands[i][0] + 1];
-            for(int j=commands[i][0]; j<=commands[i][1]; j++) {
-                slicedArr[k] = array[j-1];
-                k++;
-            }
+            int[] slicedArr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            // System.out.println(Arrays.toString(slicedArr));
+            // int k=0;
+            // int[] slicedArr = new int[commands[i][1] - commands[i][0] + 1];
+            // for(int j=commands[i][0]; j<=commands[i][1]; j++) {
+            //     slicedArr[k] = array[j-1];
+            //     k++;
+            // }
             Arrays.sort(slicedArr);
             answer[i] = slicedArr[commands[i][2]-1];
         }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
 
 ### 성능 요약
 
-메모리: 81.2 MB, 시간: 0.37 ms
+메모리: 71.5 MB, 시간: 0.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 25일 19:02:15
+2025년 03월 25일 19:17:57
 
 ### 문제 설명
 
