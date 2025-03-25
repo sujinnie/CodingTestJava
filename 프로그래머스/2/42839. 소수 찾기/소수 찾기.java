f19f8f4ce18001a0e6e72714757713cf4ae5601e
Sujin Jeong <sujin5262@naver.com>
2025-03-25 16:37:30 +0900

    [level 2] Title: 소수 찾기, Time: 25.08 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    HashSet<Integer> numberSet = new HashSet<>();
+    public int solution(String numbers) {
+        int answer = 0;
+        
+        recursive("", numbers);
+        
+        for(int num : numberSet) {
+            if(isPrime(num)) answer++;
+        }
+        
+        return answer;
+    }
+    
+    public void recursive(String comb, String others) {
+        //1. 조합을 hashset에 추가
+        if(!comb.equals(""))
+            numberSet.add(Integer.valueOf(comb));
+        //2. 숫자조각 하나씩 떼서 추가..
+        for(int i=0; i<others.length(); i++) {
+            recursive(comb+others.charAt(i), others.substring(0,i)+others.substring(i+1));   
+        }
+    }
+    
+    public boolean isPrime(int num) {
+        if(num == 0 || num == 1) return false;
+        int limit = (int) Math.sqrt(num);
+        for(int i=2; i<=limit; i++) {
+            if(num%i == 0) return false;
+        }
+        return true;
+    }
+}
