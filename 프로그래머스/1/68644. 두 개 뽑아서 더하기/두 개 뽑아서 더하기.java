cebbc4efde4975bde4233c350fd03ca4664d7f42
sujin_jeong <sujin_jeong@tmax.co.kr>
2025-04-05 16:27:17 +0900

    [chore] 프로그래머스_두개뽑아서더하기 - list 버전 풀이 추가

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -3,21 +3,31 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
         List<Integer> answer = new ArrayList<>();
-        
+
         // treeSet => 중복값 저장x, 오름차순 정렬o
         Set<Integer> set = new TreeSet<>();
         for(int i=0; i<numbers.length-1; i++) {
             for(int j=i+1; j<numbers.length; j++) {
-                set.add(numbers[i]+numbers[j]);
+                // list..
+                if(!answer.contains(numbers[i]+numbers[j])) {
+                    answer.add(numbers[i]+numbers[j]);
+                }
+
+                // set
+                // set.add(numbers[i]+numbers[j]);
             }
         }
-        
-        Iterator<Integer> iter = set.iterator();
-        while(iter.hasNext()) {
-            answer.add(iter.next());
-        }
-        
-        return answer.stream().mapToInt(Integer::intValue).toArray();
-        // return answer.stream().sorted().mapToInt(Integer::intValue).toArray();
+
+        // set
+        // Iterator<Integer> iter = set.iterator();
+        // while(iter.hasNext()) {
+        //     answer.add(iter.next());
+        // }
+
+        // list
+        return answer.stream().sorted().mapToInt(Integer::intValue).toArray();
+
+        // set
+        // return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
