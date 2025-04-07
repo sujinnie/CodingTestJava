4b3365a41f28334e5c6e8c7e83f38ec5ccf38e19
Sujin Jeong <sujin5262@naver.com>
2025-04-08 01:46:19 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.42 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/README.md
+++ 프로그래머스/0/120835. 진료 순서 정하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 진료 순서 정하기 - 120835 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120835) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120835?language=java) 
 
 ### 성능 요약
 
-메모리: 81.4 MB, 시간: 2.14 ms
+메모리: 85.5 MB, 시간: 0.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 08일 01:36:27
+2025년 04월 08일 01:46:17
 
 ### 문제 설명
 
--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -2,19 +2,37 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] emergency) {
-        Integer[] tmp = Arrays.stream(emergency).boxed().toArray(Integer[]::new);
-        Arrays.sort(tmp, Collections.reverseOrder());
-        
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<tmp.length; i++) {
-            map.put(tmp[i], i+1);
+        // int[][] arr로 풀기
+        int len = emergency.length;
+        int[] answer = new int[len];
+        int[][] map = new int[len][2];
+        for(int i=0; i<len; i++) {
+            map[i][0] = emergency[i];
+            map[i][1] = i;
         }
         
-        int[] answer = new int[emergency.length];
-        for(int i=0; i<emergency.length; i++) {
-            answer[i] = map.get(emergency[i]);
+        Arrays.sort(map, (a,b) -> b[0]-a[0]); // 응급도기준 내림차순정렬..
+        for(int i=0; i<len; i++) {
+            int originIdx = map[i][1];
+            answer[originIdx] = i+1;
         }
         
         return answer;
+        
+        // 처음생각 Map
+//         Integer[] tmp = Arrays.stream(emergency).boxed().toArray(Integer[]::new);
+//         Arrays.sort(tmp, Collections.reverseOrder());
+        
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for(int i=0; i<tmp.length; i++) {
+//             map.put(tmp[i], i+1);
+//         }
+        
+//         int[] answer = new int[emergency.length];
+//         for(int i=0; i<emergency.length; i++) {
+//             answer[i] = map.get(emergency[i]);
+//         }
+        
+//         return answer;
     }
 }
