2e2d6d35822cc6084c250f4ff86f0872cc377bc1
Sujin Jeong <sujin5262@naver.com>
2025-04-07 23:52:21 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/README.md
+++ 프로그래머스/0/181891. 순서 바꾸기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 순서 바꾸기 - 181891 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181891) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181891?language=java) 
 
 ### 성능 요약
 
-메모리: 72.3 MB, 시간: 0.03 ms
+메모리: 83.7 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 07일 23:43:07
+2025년 04월 07일 23:52:19
 
 ### 문제 설명
 
--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,13 +1,19 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
         int[] answer = new int[num_list.length];
-        int idx = 0;
-        for(int i=n; i<num_list.length; i++) {
-            answer[idx++] = num_list[i];
-        }
-        for(int i=0; i<n; i++) {
-            answer[idx++] = num_list[i];
-        }
+        System.arraycopy(num_list, n, answer, 0, num_list.length-n);
+        System.arraycopy(num_list, 0, answer, num_list.length-n, n);
         return answer;
+        
+        // int idx = 0;
+        // for(int i=n; i<num_list.length; i++) {
+        //     answer[idx++] = num_list[i];
+        // }
+        // for(int i=0; i<n; i++) {
+        //     answer[idx++] = num_list[i];
+        // }
+        // return answer;
     }
 }
