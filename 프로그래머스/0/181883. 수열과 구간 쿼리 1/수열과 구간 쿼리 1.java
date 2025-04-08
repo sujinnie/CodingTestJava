354b5b95f512ce0528bc4ab5d89e93c5762d978c
Sujin Jeong <sujin5262@naver.com>
2025-04-08 21:14:51 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.46 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -1,13 +1,12 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = arr.clone();
         for(int i=0; i<queries.length; i++) {
             int s = queries[i][0];
             int e = queries[i][1];
             for(int j=s; j<=e; j++) {
-                answer[j]++;
+                arr[j]++;
             }
         }
-        return answer;
+        return arr;
     }
 }
