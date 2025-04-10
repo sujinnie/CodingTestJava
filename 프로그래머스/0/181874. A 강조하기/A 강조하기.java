27f3d3507b1995f99f869c61a80224b14570747b
Sujin Jeong <sujin5262@naver.com>
2025-04-10 14:25:47 +0900

    [level 0] Title: A 강조하기, Time: 4.04 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for(int i=0; i<myString.length(); i++) {
+            if(myString.charAt(i) == 'a' || myString.charAt(i) == 'A') answer+='A';
+            else answer+=String.valueOf(myString.charAt(i)).toLowerCase();
+        }
+        return answer;
+    }
+}
