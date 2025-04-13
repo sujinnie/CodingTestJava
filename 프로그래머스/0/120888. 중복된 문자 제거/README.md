a106374a3c6ea3db07ead03077fcb940fd5b6283
Sujin Jeong <sujin5262@naver.com>
2025-04-14 02:02:35 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.82 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/README.md
+++ 프로그래머스/0/120888. 중복된 문자 제거/README.md
@@ -1,10 +1,10 @@
 # [level 0] 중복된 문자 제거 - 120888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120888) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120888?language=java) 
 
 ### 성능 요약
 
-메모리: 83.1 MB, 시간: 13.21 ms
+메모리: 87.7 MB, 시간: 0.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 14일 01:57:29
+2025년 04월 14일 02:02:33
 
 ### 문제 설명
 
--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,12 +1,19 @@
 import java.util.*;
+
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        for(int i=0; i<my_string.length(); i++) {
-            if(!answer.contains(String.valueOf(my_string.charAt(i)))){
-                answer+=my_string.charAt(i);
-            }
+        String[] splitted = my_string.split("");
+        Set<String> set = new LinkedHashSet<>();
+        for(String str : splitted) {
+            set.add(str);
         }
+        answer = String.join("", set);
+        // for(int i=0; i<my_string.length(); i++) {
+        //     if(!answer.contains(String.valueOf(my_string.charAt(i)))){
+        //         answer+=my_string.charAt(i);
+        //     }
+        // }
         return answer;
     }
 }
