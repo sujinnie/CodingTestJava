737247bcd6bfe002bdb55236cb889f2e34e82f01
Sujin Jeong <sujin5262@naver.com>
2025-04-14 01:05:41 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.47 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,16 +1,32 @@
 import java.util.*;
+
 class Solution {
     public int solution(String s) {
         int answer = 0;
         String[] arr = s.split(" ");
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i].equals("Z")) {
-                answer -= Integer.parseInt(arr[i-1]);
+        
+        // Stack
+        Stack<Integer> stk = new Stack<>();
+        for(String str : arr) {
+            if(str.equals("Z")) {
+                stk.pop();
             }
             else {
-                answer+= Integer.parseInt(arr[i]);
+                stk.push(Integer.parseInt(str));
             }
         }
+        for(int num : stk) {
+            answer+=num;
+        }
+        
+        // for(int i=0; i<arr.length; i++) {
+        //     if(arr[i].equals("Z")) {
+        //         answer -= Integer.parseInt(arr[i-1]);
+        //     }
+        //     else {
+        //         answer+= Integer.parseInt(arr[i]);
+        //     }
+        // }
         return answer;
     }
 }
