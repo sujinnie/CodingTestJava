48d30e384544757424deb18bbd715d94ff9a7ad1
Sujin Jeong <sujin5262@naver.com>
2025-04-15 18:04:57 +0900

    [level 0] Title: 배열 비교하기, Time: 2.05 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        int len1 = arr1.length;
+        int len2 = arr2.length;
+        
+        if(len1 > len2) return 1;
+        else if(len1 < len2) return -1;
+        else {
+            int sum1 = Arrays.stream(arr1).sum();
+            int sum2 = Arrays.stream(arr2).sum();
+            if(sum1 > sum2) return 1;
+            else if(sum1 < sum2) return -1;
+            else return 0;
+        }
+    }
+}
