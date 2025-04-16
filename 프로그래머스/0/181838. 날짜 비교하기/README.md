e87a1fd04afcda68e76f371ac8a763e4e3e4616b
Sujin Jeong <sujin5262@naver.com>
2025-04-17 04:13:12 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.07 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/README.md
+++ 프로그래머스/0/181838. 날짜 비교하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 날짜 비교하기 - 181838 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181838) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181838?language=java) 
 
 ### 성능 요약
 
-메모리: 78.3 MB, 시간: 0.02 ms
+메모리: 77.6 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 17일 04:00:20
+2025년 04월 17일 04:13:10
 
 ### 문제 설명
 
--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,17 +1,35 @@
+// import java.time.LocalDate;
+import java.util.*;
+
 class Solution {
-    public int solution(int[] date1, int[] date2) {
-        int year1 = date1[0], mon1 = date1[1], day1 = date1[2];
-        int year2 = date2[0], mon2 = date2[1], day2 = date2[2];
+    public int solution(int[] date1, int[] date2) {   
+        // Arrays.compare(java9 이상)
+        // return 1:1>2 / 0:1=2 / -1:1<2
+        return Arrays.compare(date1, date2) == -1 ? 1 : 0;
         
-        if(year1 < year2) return 1;
-        else if(year1 > year2) return 0;
-        else {
-            if(mon1 < mon2) return 1;
-            else if(mon1 > mon2) return 0;
-            else {
-                if(day1 < day2) return 1;
-                else return 0;
-            }
-        }
+        // ㅠㅠ
+//         int year1 = date1[0], mon1 = date1[1], day1 = date1[2];
+//         int year2 = date2[0], mon2 = date2[1], day2 = date2[2];
+        
+//         if(year1 < year2) return 1;
+//         else if(year1 > year2) return 0;
+//         else {
+//             if(mon1 < mon2) return 1;
+//             else if(mon1 > mon2) return 0;
+//             else {
+//                 if(day1 < day2) return 1;
+//                 else return 0;
+//             }
+//         }
+        
+        // LocalDate ..
+//         LocalDate dateA = LocalDate.of(date1[0], date1[1], date1[2]);
+//         LocalDate dateB = LocalDate.of(date2[0], date2[1], date2[2]);
+
+//         if (dateA.isBefore(dateB)) {
+//             return 1;
+//         } else {
+//             return 0;
+//         }
     }
 }
