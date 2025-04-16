e50b20e8e0ebbec2b540c537cb9db150078d0a80
Sujin Jeong <sujin5262@naver.com>
2025-04-17 02:15:51 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 12.78 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/README.md
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/README.md
@@ -1,10 +1,10 @@
 # [level 0] 한 번만 등장한 문자 - 120896 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120896) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120896?language=java) 
 
 ### 성능 요약
 
-메모리: 82.6 MB, 시간: 2.61 ms
+메모리: 91.1 MB, 시간: 12.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 17일 02:10:34
+2025년 04월 17일 02:15:49
 
 ### 문제 설명
 
--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -2,16 +2,30 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        Map<String, Integer> map = new TreeMap<>();
-        for(String str : s.split("")) {
-            map.put(str, map.getOrDefault(str, 0) + 1);
+        // cnt 배열
+        int[] cnt = new int[26];
+        for(char c : s.toCharArray()) {
+            cnt[c-'a']++; 
         }
         
         String answer = "";
-        for(Map.Entry<String, Integer> entry : map.entrySet()) {
-            if(entry.getValue() == 1) answer += entry.getKey();
+        for(int i=0; i<26; i++) {
+            if(cnt[i] == 1) answer += (char)(i+'a');
         }
         
         return answer;
+        
+        // treeMap
+//         Map<String, Integer> map = new TreeMap<>();
+//         for(String str : s.split("")) {
+//             map.put(str, map.getOrDefault(str, 0) + 1);
+//         }
+        
+//         String answer = "";
+//         for(Map.Entry<String, Integer> entry : map.entrySet()) {
+//             if(entry.getValue() == 1) answer += entry.getKey();
+//         }
+        
+//         return answer;
     }
 }
